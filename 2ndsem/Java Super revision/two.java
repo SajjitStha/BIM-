0c6444ab80ad202b77099701bd09f9a1987243d4
SajjitStha <sajjitshrestha@gmail.com>
2024-07-03 12:34:21 +0545

    Unwanted files Removed

--- 2ndsem/Java Super revision/two.java
+++
@@ -1,9 +0,0 @@
-public class two {
-    public static void main(String[] args)
-    {
-        System.out.println("Name:Sajjit");
-        System.out.println("Roll:44");
-        System.out.println("Address:Chabahil");
-        System.out.println("Phone Number:9861432490");
-    }
-}
